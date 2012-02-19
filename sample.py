e7f0cd05a021470128f631d96fa1af949ecb83e6
Christian Johnson <christian@christianjohnson.org>
2012-02-18 20:28:14 -0500

    Sample

--- sample.py
+++ sample.py
@@ -1,7 +1,25 @@
 # Christian Johnson
 # NLTK Basic Processing
+#Uses http://text-processing.com/docs/sentiment.html API
+
+'''
+
+Sample output:
+
+Weekend Investor: 5 game changers for small-cap stocks: Overall neutral, Positive 38.98, Negative: 61.02, Nuetral: 74.02
+NewsWatch: U.S. stock indexes end with weekly rise: Overall neutral, Positive 66.49, Negative: 33.51, Nuetral: 66.39
+NewsWatch: U.S. stock indexes end with weekly rise: Overall neutral, Positive 66.49, Negative: 33.51, Nuetral: 66.39
+Asia Markets: China data, Esprit earnings ahead in Asia: Overall neutral, Positive 62.27, Negative: 37.73, Nuetral: 51.46
+Auto Review: Mazda 3 a sporty choice as gas prices rise: Overall neutral, Positive 52.04, Negative: 47.96, Nuetral: 67.71
+Energy Stocks: First Solar, $103 crude rally energy stocks: Overall neg, Positive 39.63, Negative: 60.37, Nuetral: 9.12
+Latin American Markets: Brazilian stocks edge up ahead of holiday break: Overall neutral, Positive 70.16, Negative: 29.84, Nuetral: 85.28
+Applied shares ease after rising on results: Overall neutral, Positive 40.50, Negative: 59.50, Nuetral: 74.58
+
+'''
 
 #Needs the feedparser library and simplejson library to function.
+# http://code.google.com/p/feedparser/
+# http://pypi.python.org/pypi/simplejson/
 
 import pprint
 import urllib
