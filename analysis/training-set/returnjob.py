cbb65c9648b269267e7ee315f1a01d5a9cc085a3
Michael <michorowitz@gmail.com>
2012-03-21 14:47:39 -0400

    Updated returnjob - it now works.

--- analysis/training-set/returnjob.py
+++ analysis/training-set/returnjob.py
@@ -8,6 +8,12 @@ class ReturnJob(object):
   @cherrypy.expose
   @cherrypy.tools.json_out()
   def index(self, articleid, score):
+    try:
+      articleid = int(articleid)
+      score = int(score)
+    except:
+      return "FAILURE"
+
     conn = MySQLdb.connect(**self.dbinfo)
 
     cur = conn.cursor()
@@ -15,10 +21,16 @@ class ReturnJob(object):
     if score not in (-1,0,1):
       return "FAILURE"
 
+    if score == -1:
+      field = "trainneg"
+    elif score == 0:
+      field = "trainneutral"
+    elif score == 1:
+      field = "trainpos"
+
     #Get the id and the paragraph for all elements with an empty score
-    #sql = "UPDATE trainset SET score=%s WHERE id=%s"
-    sql = "UPDATE articles SET score=%s WHERE id=%s"
-    args = score,articleid
+    sql = "UPDATE articles SET " + field + "=" + field + "+1 WHERE id=%s"
+    args = articleid
     cur.execute(sql, args)
 
     cur.close()
