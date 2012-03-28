cd5c668c0546eb3a1bd6fc6a55d5e86d74afbbe6
Christian Johnson <christian@christianjohnson.org>
2012-03-28 13:48:49 -0400

    Search

--- server/model.py
+++ server/model.py
@@ -10,16 +10,11 @@ else:
       user=config.mysql_db_user, \
       pw=config.mysql_db_password)
 
-def get_todos():
-  return db.select('todo', order='id')
-
-def new_todo(text):
-  db.insert('todo', title=text)
-
-def del_todo(id):
-  db.delete('todo', where="id=$id", vars=locals())
-
-def get_articles():
+def get_articles(q):
+  if q:
+   return db.query("SELECT * FROM articles WHERE LOWER(title) LIKE LOWER($query) ORDER BY time_crawled DESC LIMIT 50", vars=dict(query='%' + q + '%'))
+   #Tried doing it with arguments, didn't work well.
+   #return db.select('artcles', where="LOWER(title) LIKE LOWER($query)", order="time_crawled DESC", limit=50, vars=dict(query='%' + q + '%'))
   return db.select('articles', order='time_crawled DESC', limit=50)
   
 def new_article(url, title, text, pos, neg, neu):
